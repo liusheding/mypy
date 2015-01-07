cc610e6479aabe80e657f7fc511e2effd4fcb38f
liusheding <liusheding@gmail.com>
2015-01-07 23:14:15 +0800

    ddddd

---
+++ py/Hello.py
@@ -0,0 +1,17 @@
+'''
+Created on 2015Äê1ÔÂ7ÈÕ
+
+@author: lenovo
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
